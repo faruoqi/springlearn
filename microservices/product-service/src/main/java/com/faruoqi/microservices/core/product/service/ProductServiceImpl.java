c099061eb8dddc9fe9bf8e10db3791578708ba64
faruoqi <rezafaruoqi@gmail.com>
2024-06-13 15:01:20 +0700

    add multiple services

---
+++ microservices/product-service/src/main/java/com/faruoqi/microservices/core/product/service/ProductServiceImpl.java
@@ -0,0 +1,24 @@
+package com.faruoqi.microservices.core.product.service;
+
+import com.faruoqi.api.core.product.Product;
+import com.faruoqi.util.http.ServiceUtil;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ProductServiceImpl {
+
+    private final ServiceUtil serviceUtil;
+
+    @Autowired
+    public ProductServiceImpl(ServiceUtil serviceUtil){
+        this.serviceUtil = serviceUtil;
+    }
+
+    public Product getProduct(int productId){
+        return new Product(productId,"name-"+productId,123,serviceUtil.getServiceAddress());
+
+    }
+
+
+}
