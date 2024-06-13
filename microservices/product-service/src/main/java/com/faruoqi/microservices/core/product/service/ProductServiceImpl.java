fc08ac5a877e344aef9fe074f79ddc556583a25f
faruoqi <rezafaruoqi@gmail.com>
2024-06-13 15:28:12 +0700

    edit product service impl

--- microservices/product-service/src/main/java/com/faruoqi/microservices/core/product/service/ProductServiceImpl.java
+++ microservices/product-service/src/main/java/com/faruoqi/microservices/core/product/service/ProductServiceImpl.java
@@ -1,12 +1,13 @@
 package com.faruoqi.microservices.core.product.service;
 
 import com.faruoqi.api.core.product.Product;
+import com.faruoqi.api.core.product.ProductService;
 import com.faruoqi.util.http.ServiceUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class ProductServiceImpl {
+public class ProductServiceImpl implements ProductService {
 
     private final ServiceUtil serviceUtil;
 
